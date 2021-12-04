ecd0afa81eb9b5fa8c0fbd85f6abfbe5ecb221c5
rxt30 <mhampel@pm.me>
2021-12-04 18:18:39 +0100

    Added second solution for the fourth day

--- 04/bingo.py
+++ 04/bingo.py
@@ -8,6 +8,15 @@ def checkWin(playSequence, playFields):
             return True, number
     return False, 0
 
+def checkLastWin(playSequence, playFields):
+    for playField in playFields:
+        win, number = playField.checkMatrix(playSequence)
+        if(win) and len(playFields) != 1:
+            playFields.remove(playField)
+        elif(win) and len(playFields) == 1:
+            return True, number, playFields
+    return False, 0, playFields
+
 def findWinner(playSequence, playFields):
     for i in range(5,len(playSequence)):
         winFound, number = checkWin(playSequence[0:i], playFields)
@@ -15,5 +24,13 @@ def findWinner(playSequence, playFields):
             return number
     return 'nothing else matters'
 
+def findLastWinner(playSequence, playFields):
+    for i in range(5,len(playSequence)):
+        lastWinFound, number, playFields = checkLastWin(playSequence[0:i], playFields)
+        if(lastWinFound):
+            return number
+    return 'nothing else matters'
+
 playSequence, playFields = readFile('./input0401.txt')
 print("The number is: " + str(findWinner(playSequence, playFields)))
+print("The number is: " + str(findLastWinner(playSequence, playFields)))
